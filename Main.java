471b41d23a84cd097e1f0d543fcfab9606b754ad
Pavan8518 <91248254+Sharad8518@users.noreply.github.com>
2022-02-13 13:17:11 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,40 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.Scanner;
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    
+	    Scanner in =new Scanner(System.in);
+	    
+	    int a,b=1,c=0,no;
+	    
+	    
+	    System.out.println("Enter any Number upto you want to print Fibonacci series");
+	
+	
+	   no =in.nextInt();
+	   
+	   for(int i=1;i<=no;i++){
+	       
+	   System.out.println(c+"");
+	   
+	   a=b;
+	   b=c;
+	   c=a+b;
+	       
+	   }
+	
+	
+	
+	
+	}
+}
